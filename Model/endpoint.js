cbe8a9fdb409adce048f4eca7697114ac04b2f5c
marshall33 <sudeepshrestha33@gmail.com>
2024-10-29 10:00:14 +0100

    assignment completed

---
+++ Model/endpoint.js
@@ -0,0 +1,5 @@
+function endpoint() 
+{
+    return "Hello from the Endpoint";
+}
+module.exports = endpoint;
---
+++ Model/server.js
@@ -0,0 +1,9 @@
+const endpoint = require("./endpoint");
+function main()
+{
+    console.log('Hello World');
+    console.log(endpoint());
+}
+
+main();
+endpoint();
